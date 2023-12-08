a905b6b652611009bf0dce0b247c257565d13e88
Rogério <146471734+rogerioalves-sf@users.noreply.github.com>
2023-12-08 16:57:45 -0300

    formulário numeros finalizado

---
+++ exformularioJS/js/script.js
@@ -0,0 +1,16 @@
+
+const nA = document.querySelector("#txtn")
+const nB = document.querySelector("#txtn2")
+const form = document.querySelector("#form")
+
+form.addEventListener("submit",(event)=> {
+    event.preventDefault();
+    if (nA.value > nB.value) {
+        erron1.style.display = "block";
+        sucesso.style.display = "none";
+        return;
+    } else {
+        sucesso.style.display = "block";
+        erron1.style.display = "none";
+    }
+})
