c8333290f259fa4730387b29f699ade5f0c0de6e
Rogério Alves <146471734+rogerioalves-sf@users.noreply.github.com>
2024-03-15 16:05:14 -0300

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+        development: {
+            files: {
+                'dev/styles/main.min.css': 'src/styles/main.less'
+            }
+        },
+        production: {
+            options: {
+                compress: true,
+            },
+            files: {
+                'dist/styles/main.min.css': 'src/styles/main.less'
+            }
+        }
+    },
+    replace: {
+        dev: {
+            options: {
+                patterns: [
+                    {
+                        match: 'ENDERECO_DO_CSS',
+                        replacement: './styles/main.css'
+                    },
+                    {
+                        match: 'ENDERECO_DO_JS',
+                        replacement: '../src/scripts/main.js'
+                    }
+                ]
+            },
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['src/index.html'],
+                    dest: 'dev/'
+                }
+            ]
+        },
+        dist: {
+            options: {
+                patterns: [
+                    {
+                        match: 'ENDERECO_DO_CSS',
+                        replacement: './styles/main.min.css'
+                    },
+                    {
+                        match: 'ENDERECO_DO_JS',
+                        replacement: './scripts/main.min.js'
+                    }
+                ]
+            },
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['prebuild/index.html'],
+                    dest: 'dist/'
+                }
+            ]
+        }
+    },
+    uglify: {
+        target: {
+            files: {
+                'dist/scripts/main.min.js':'src/scripts/main.js'
+            }
+        }
+    }
+})
+
+
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-replace');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+grunt.registerTask('default', ['watch'] );
+grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify'] );
